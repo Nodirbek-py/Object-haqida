2a434609721dabf45bea755843153ab44657fb6b
nodirbek-py <nodvos@gmail.com>
2021-02-26 22:08:23 +0500

    Object va Array

---
+++ script.js
@@ -0,0 +1,21 @@
+var shaxs = {
+    "ism":"Palonchi",
+    "fam":"Pistonchiyev",
+    "yosh": 27,
+    "interest": ["sport", "ilm-fan", "..."],
+    "occupation": {
+        "kasb": "injener",
+        "korxona": {
+            "nomi": "blah blah",
+            "founded": 1994,
+            "region": {
+                "nomi": "Andijon",
+                "aholi": "3 million"
+
+            }
+        },
+        "staj": 5
+    }
+}
+
+console.log(shaxs["interest"][1])
